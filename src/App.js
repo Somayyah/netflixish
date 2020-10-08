11165190a861448b4c7c7cb7bc16046ade50a149
somayyah <smalfraihat131@cit.just.edu.jo>
2020-10-08 21:18:49 +0300

    app.css :x: Row.js :heavy_plus_sign:

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import './App.css';
+import Row from './components/Row';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Row title="Netflix Originals" />
+      <Row title="Netflix Originals" />
+      <Row title="Netflix Originals" />
+      <Row title="Netflix Originals" />
+    </div>
   );
 }
 
---
+++ src/components/Row.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Row(){
+	return(
+		<h1>Component Created Successfully :)</h1>
+	)
+}
+
+export default Row;
