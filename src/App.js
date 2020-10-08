4399c328df74c0a51713a8926299f16e8f574718
somayyah <smalfraihat131@cit.just.edu.jo>
2020-10-08 22:39:26 +0300

    dummy files :x:

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Row from './components/Row';
+import './styles/main.css';
 
 function App() {
   return (
     <div className="App">
       <Row title="Netflix Originals" />
-      <Row title="Netflix Originals" />
-      <Row title="Netflix Originals" />
-      <Row title="Netflix Originals" />
+      <Row title="Trending Now" />
+      <Row title="Top Rated" />
+      <Row title="Action Movies" />
+      <Row title="Comedy Movies" />
     </div>
   );
 }
