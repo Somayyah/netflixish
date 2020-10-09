a9ee423e210d6bc71511b5e21cd87ee4ef328e26
somayyah <smalfraihat131@cit.just.edu.jo>
2020-10-09 19:06:57 +0300

    ignore env

--- src/requests.js
+++ src/requests.js
@@ -1,14 +1,15 @@
-const API_KEY = process.env.API_KEY;
-
+const env = require('dotenv').config()
+const api_key = process.env.API_KEY;
+console.log(api_key)
 const requests = {
-    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-us`,
-    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&language=en-us`,
-    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-us`,
-    fetchActionMovis: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-    fetchComedyMovis: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-    fetchHorrorMovis: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-    fetchRomanticMovis: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    fetchTrending: `/trending/all/week?api_key=${api_key}&language=en-us`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${api_key}&language=en-us`,
+    fetchTopRated: `/movie/top_rated?api_key=${api_key}&language=en-us`,
+    fetchActionMovis: `/discover/movie?api_key=${api_key}&with_genres=28`,
+    fetchComedyMovis: `/discover/movie?api_key=${api_key}&with_genres=35`,
+    fetchHorrorMovis: `/discover/movie?api_key=${api_key}&with_genres=27`,
+    fetchRomanticMovis: `/discover/movie?api_key=${api_key}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${api_key}&with_genres=99`,
 
 }
 
