b2d227be44bcafd5986debf541da7022e23fb2c3
somayyah <smalfraihat131@cit.just.edu.jo>
2020-10-12 20:02:32 +0300

    purgecss applied

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,17 @@ const tailwindcss = require('tailwindcss');
 module.exports = {
     plugins: [
         tailwindcss('./tailwind.config.js'),
-        require('autoprefixer')
+        require('autoprefixer'),
+				require('@fullhuman/postcss-purgecss')({
+								content: [
+												'./src/components/*.js',
+												'./public/*.js',
+												'./src/views/Home.js',
+												'./src/*.js',
+												'./public/index.html',
+								],
+								defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]*/g) || []
+				})
     ],
 };
 
