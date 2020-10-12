51dfd5adf640817b73dc99403d447143c7358897
somayyah <smalfraihat131@cit.just.edu.jo>
2020-10-12 19:14:04 +0300

    tab added

--- src/views/Home.js
+++ src/views/Home.js
@@ -15,7 +15,7 @@ function Home() {
 					isLargeRow
 					fetchUrl={requests.fetchNetflixOriginals}
 				/>
-								<Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+				<Row title="Trending Now" fetchUrl={requests.fetchTrending} />
 				<Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
 				<Row title="Action Movies" fetchUrl={requests.fetchActionMovis} />
 				<Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovis} />
